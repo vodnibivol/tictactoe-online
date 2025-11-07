2300fc23955d1749818b2f3287a339a97adad798
vodnibivol <vodnibivol@gmail.com>
2025-11-07 22:59:30 +0100

    still trying to fix the socket-io url setup

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, { path: '/tictactoe/socket.io/' });
+const io = new Server(server, { path: '/socket.io/' });
 
 app.use(compression());
 
